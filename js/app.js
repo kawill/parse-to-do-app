8bdb2c6ecbfd726aa07f8783efcfbe5da32915f9
Kristen <kawilliams3@gmail.com>
2015-02-26 15:09:24 -0600

    ugh

--- js/app.js
+++ js/app.js
@@ -13,9 +13,9 @@ function app(){
         {url: "./bower_components/lodash/lodash.min.js"},
 
         // when using just Backbone, use this line
-        {url: "./bower_components/backbone/backbone.js"},
+        // {url: "./bower_components/backbone/backbone.js"},
         // when using Parse, comment out the above line and uncomment the line below
-        // {url: "./bower_components/parse-js-sdk/lib/parse.min.js"},
+        {url: "./bower_components/parse-js-sdk/lib/parse.min.js"},
 
         // when using React (and the plugin JSnoX), uncomment the following two lines
         // {url: "./bower_components/react/react.min.js"},
@@ -23,10 +23,13 @@ function app(){
 
         // other stuff
         {url: "./bower_components/pace/pace.min.js"},
-        {url: "./js/TemplateView.js"}
+        {url: "./js/TemplateView.js"},
+        {url: "./js/toDoApp.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
+        Parse.initialize("vfHPmtj4mnPCQm9KH0iqQs5oMpXFM8vg5kyJscie", "auYNghv4QvqxuINK1biI42mlrbSvSvVs6PaPBpBn"); //app ID & JS Key
         // start app?
+        new Parse.TodoRouter();
     })
 
 }
